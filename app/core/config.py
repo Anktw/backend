d1e1b275ba66a995b343655bb6db52b0c5cce93b
Ankit Tiwari <140378286+Anktw@users.noreply.github.com>
2025-04-19 01:34:38 +0530

    removed docker for local redis environment

--- app/core/config.py
+++ app/core/config.py
@@ -13,10 +13,11 @@ class Settings(BaseSettings):
 
     REDIS_HOST: str
     REDIS_PORT: int
+    REDIS_PASSWORD: str
 
     model_config = SettingsConfigDict(
         env_file=".env",        
         from_attributes=True      
     )
-    
+
 settings = Settings()
--- app/services/redis_otp.py
+++ app/services/redis_otp.py
@@ -5,7 +5,7 @@ from app.core.config import settings
 r = redis.Redis(
     host=settings.REDIS_HOST,
     port=settings.REDIS_PORT,
-    password=REDIS_PASSWORD,
+    password=settings.REDIS_PASSWORD,
     db=0,
     decode_responses=True
 )
