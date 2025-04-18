0a03d4894e6e211f0c86d6792068e204ff394b1c
Ankit Tiwari <140378286+Anktw@users.noreply.github.com>
2025-04-19 01:19:05 +0530

    preparing for deployment

--- app/services/redis_otp.py
+++ app/services/redis_otp.py
@@ -5,6 +5,7 @@ from app.core.config import settings
 r = redis.Redis(
     host=settings.REDIS_HOST,
     port=settings.REDIS_PORT,
+    password=REDIS_PASSWORD,
     db=0,
     decode_responses=True
 )
