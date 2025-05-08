9081bb7e042454dc9170c63e9c96a94b8891cc0a
Ankit Tiwari <140378286+Anktw@users.noreply.github.com>
2025-05-08 22:45:29 +0530

    adding connection request to postgre  more precise

--- app/main.py
+++ app/main.py
@@ -1,9 +1,29 @@
+import time
+from sqlalchemy import text
+from sqlalchemy.exc import OperationalError
+from app.db.session import engine
 from fastapi import FastAPI
 from app.api.v1 import auth, admin, user, social_auth
 from starlette.middleware.sessions import SessionMiddleware
 from app.core.config import settings
 
 app = FastAPI()
+
+@app.on_event("startup")
+def wait_for_db():
+    retries = 10
+    while retries > 0:
+        try:
+            with engine.connect() as conn:
+                conn.execute(text("SELECT 1"))
+            print("Database is ready!")
+            break
+        except OperationalError:
+            print("Database not ready, waiting...")
+            time.sleep(2)
+            retries -= 1
+    else:
+        raise Exception("Database connection failed after retries")
 
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
 app.include_router(admin.router, prefix="/admin", tags=["admin"])
